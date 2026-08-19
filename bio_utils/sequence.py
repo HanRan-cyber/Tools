42d68c688944d8a24d4aaabefb91c725f191a77b
HanRan <2892026179@qq.com>
2026-08-19 21:24:30 +0800

    编写：已完成read_fasta函数

--- bio_utils/sequence.py
+++ bio_utils/sequence.py
@@ -6,7 +6,33 @@ def read_fasta(filepath):
     返回:
         dict
     """
-    pass  # TODO: 实现
+    result = {}  #准备一个空字典，用于储存结果
+    current_id = None #存放当前序列的ID
+    current_seq = [] #存放当前序列
+    file = open(filepath, "r") #读取文件。注借助with open(filepath, "r") as f:的写法可不用f.close
+    #一行一行地读取文件内容
+    for line in file:
+        #预处理数据
+        line = line.strip()
+        if not line:
+            continue
+        elif line.startswith(";"):  #startswith() 用于判断字符串是否以指定前缀开头
+            continue
+        elif line.startswith(">"):  #startswith() 用于判断字符串是否以指定前缀开头
+            if not current_id:
+                current_id = line[1:].split()[0]
+                #split函数用于把字符串按分隔符拆成列表,括号内可添加指定分隔符，在此用于去除注释
+            else:
+                result[current_id] = "".join(current_seq)
+                current_seq = []
+                current_id = line[1:].split()[0]  #split函数用于把字符串按分隔符拆成列表，在此用于去除注释
+        else:
+            current_seq.append(line)
+    if current_id is not None:
+        result[current_id] = "".join(current_seq) #解决最后一段序列无法添加到字典的问题
+    file.close() #关闭文件，释放内存，千万不要忘记
+    return result
+
 
 def gc_content(seq):
     """
