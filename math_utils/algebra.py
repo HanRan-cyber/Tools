14f7fd5a6a114d5b10c3787659836207abcb50b8
HanRan <2892026179@qq.com>
2026-08-19 23:08:47 +0800

    编写：已完成linear_regression函数

--- math_utils/algebra.py
+++ math_utils/algebra.py
@@ -6,7 +6,25 @@ def linear_regression(x,y):
     返回：
         (slope, intercept)
     """
-    pass #TODO
+    sum_x = 0  #用于计算列表x的和
+    sum_y = 0  #用于计算列表y的和
+    for num_x in x:  #计算x的平均数
+        sum_x += num_x
+    avg_x = sum_x / len(x)
+    for num_y in y:  #计算y的平均数
+        sum_y += num_y
+    avg_y = sum_y / len(y)
+    count_xy = 0
+    count_x = 0
+    for xi, yi in zip (x, y):  #zip函数用于将两个数据变成组元
+        count_xy += (xi - avg_x) * (yi - avg_y)
+        count_x += (xi - avg_x) ** 2
+    slope = count_xy / count_x  #计算斜率
+    intercept = avg_y - slope * avg_x  #计算截距
+    return slope, intercept
+
+
+
 
 def pearson_correlation(x, y):
     """
