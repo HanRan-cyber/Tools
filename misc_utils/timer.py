40a1e05e98e867ef57e658ffd9679d4243dc5c1c
HanRan <2892026179@qq.com>
2026-08-20 22:17:13 +0800

    编写：已完成timer函数

--- misc_utils/timer.py
+++ misc_utils/timer.py
@@ -1,9 +1,13 @@
 import time
 
-def timer(func):
+def timer(func):  #装饰器的写法需要熟练掌握
     """
     装饰器：打印函数执行耗时
     """
     def wrapper(*args, **kwargs):
-        pass  # TODO: 实现
+        start = time.perf_counter() #开始计时
+        result = func(*args, **kwargs)  #真正执行原函数，*按位置，**带名字
+        end = time.perf_counter()  #结束计时
+        print(f'{func.__name__}耗时：{end - start}秒')  #格式化写法
+        return result
     return wrapper
