4dcb09ab731769b77da600b7d9a9e89b2b643a13
HanRan <2892026179@qq.com>
2026-08-20 10:53:38 +0800

    编写：已完成peason_correlation函数

--- math_utils/algebra.py
+++ math_utils/algebra.py
@@ -14,12 +14,12 @@ def linear_regression(x,y):
     for num_y in y:  #计算y的平均数
         sum_y += num_y
     avg_y = sum_y / len(y)
-    count_xy = 0
-    count_x = 0
+    cov_xy = 0  #cov表示协方差
+    var_x = 0  #var表示方差
     for xi, yi in zip (x, y):  #zip函数用于将两个数据变成组元
-        count_xy += (xi - avg_x) * (yi - avg_y)
-        count_x += (xi - avg_x) ** 2
-    slope = count_xy / count_x  #计算斜率
+        cov_xy += (xi - avg_x) * (yi - avg_y)
+        var_x += (xi - avg_x) ** 2
+    slope = cov_xy / var_x  #计算斜率
     intercept = avg_y - slope * avg_x  #计算截距
     return slope, intercept
 
@@ -28,10 +28,34 @@ def linear_regression(x,y):
 
 def pearson_correlation(x, y):
     """
-    计算皮尔逊相关系数
+    计算皮尔逊相关系数（即相关系数r）
     参数:
         x, y: list of numbers
     返回:
         float or None （如果长度<2）
     """
-    pass #TODO
+    if len(x) < 2 or len(y) < 2 or len(x) != len(y):  #先对两个列表进行边界处理
+        return None
+    sum_x = 0  #用于计算列表x的和
+    sum_y = 0  #用于计算列表y的和
+    for num_x in x:  #计算x的平均数
+        sum_x += num_x
+    avg_x = sum_x / len(x)
+    for num_y in y:  #计算y的平均数
+        sum_y += num_y
+    avg_y = sum_y / len(y)
+    cov_xy = 0
+    var_x = 0
+    var_y = 0
+    for xi, yi in zip (x, y):  #zip函数用于将两个数据变成组元
+        cov_xy += (xi - avg_x) * (yi - avg_y)
+        var_x += (xi - avg_x) ** 2
+        var_y += (yi - avg_y) ** 2
+    if var_x * var_y == 0:  #无波动时相关系数为0
+        return None
+    pearson = cov_xy / ((var_x * var_y)**0.5)
+    if pearson > 1.0:  #处理浮点误差
+        pearson = 1.0
+    elif pearson < -1.0:
+        pearson = -1.0
+    return pearson
