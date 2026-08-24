7c73061a05d5945a7f1537db422936a8d6f8d062
HanRan <2892026179@qq.com>
2026-08-24 19:50:22 +0800

    修改：完善函数import

--- bio_utils/__init__.py
+++ bio_utils/__init__.py
@@ -1,3 +1,5 @@
 from .sequence import read_fasta, gc_content, reverse_complement
+from .kimetics import enzyme_kinetics
+from .alignment import needleman_wunsch
 
-__all__ = ['read_fasta', 'gc_content', 'reverse_complement']
+__all__ = ['read_fasta', 'gc_content', 'reverse_complement', 'enzyme_kinetics', 'needleman_wunsch']
