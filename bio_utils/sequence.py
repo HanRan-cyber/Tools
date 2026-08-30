5776b87bbcc56f1751402d7b0d2ae708304f8d78
HanRan <2892026179@qq.com>
2026-08-30 21:37:00 +0800

    部分编写：完成translate的部分内容

--- bio_utils/sequence.py
+++ bio_utils/sequence.py
@@ -1,4 +1,24 @@
-from misc_utils import *
+from misc_utils import timer
+
+#该字典用于mRNA的翻译
+CODON_TABLE = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W',
+}
 
 @timer  #装饰器的运用
 def read_fasta(filepath):
@@ -91,5 +111,34 @@ def gc_window(seq, k):
         gc_window_content = gc_content(seq[i:i+k])  #调用之前的函数完成gc含量的计算
         window_content.append(gc_window_content)
     return window_content
+
+
+def translate(seq, frame: int = 1, reverse: bool = False, stop: str = '*') :
+#学习一下这种写法！例如frame: int = 1 中int为提示类型，=1是默认值。调用时不赋值就默认为一，若要赋值则可这样操作：translate(sqe, frame = 2)
+    """
+    将 mRNA 序列翻译为蛋白质。
+
+    参数：
+        seq: DNA 序列字符串（只含 A/T/C/G，大小写均可）
+        frame: 读码框，可选 1, 2, 3（从第几个碱基开始，1-based）
+        reverse: 若为 True，则先进行反向互补再翻译
+        stop: 终止密码子的表示方式，'*' 表示终止符；若设为 None 或 False，则遇到终止密码子提前停止翻译
+
+    返回：
+        翻译得到的蛋白质字符串
+    """
+    seq = seq.upper  #数据的预处理
+    peptide_chain = []
+    for item in range(frame,len(seq) + 1, 3):
+        codon = seq[item:item + 3]
+        if codon not in CODON_TABLE:
+            raise ValueError(f'{codon}中有错误字母，请检查后再上传')
+        else:
+            amino_acid = CODON_TABLE[codon]
+            peptide_chain.append(amino_acid)
+        if reverse:
+            pass #TODO:明天再写
+
+
 
 
