c0fe705fb36bb0121f70c73b47d04525ef3aacce
HanRan <2892026179@qq.com>
2026-09-01 10:33:35 +0800

    部分编写：增添translate函数的调用

--- bio_utils/__init__.py
+++ bio_utils/__init__.py
@@ -2,6 +2,7 @@ from .sequence import read_fasta, gc_content, reverse_complement, translate
 from .kimetics import enzyme_kinetics
 from .alignment import needleman_wunsch
 
+
 __all__ = [
     'read_fasta', 'gc_content', 'reverse_complement', 'enzyme_kinetics', 'needleman_wunsch',
     'translate'
